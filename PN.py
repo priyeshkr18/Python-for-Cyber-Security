87598be17237ff38b53ae147cab065416f36682a
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-01-06 01:42:47 +0530

    Add files via upload

---
+++ PN.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+#checking if number is positive, negative or 0
+if(num > 0):
+  print("Positive")
+elif(num < 0):
+  print("Negative")
+elif(num == 0):
+  print("Zero")
+else:
+  print("Invalid input")
