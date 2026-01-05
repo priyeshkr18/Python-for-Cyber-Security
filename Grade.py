53b0b46956cacd4334401f774d41b0e5368d872e
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-01-06 01:34:06 +0530

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,19 @@
+print("Welcome! Student")
+marks = int(input("Enter your marks: "))
+if(marks <= 30):
+  print("Fail!")
+elif(marks > 30 or marks < 45):
+  print("Grade D")
+elif(marks > 46 or marks < 60):
+  print("Grade C")
+elif(marks > 61 or marks < 75):
+  print("Grade B")
+elif(makrs > 76 or marks < 85):
+  print("Grade A")
+elif(marks > 86 or marks < 99):
+  print("Grade O")
+else:
+  print("Invalid input")
+  
+print("Thank you! Best Wishes")
+  
